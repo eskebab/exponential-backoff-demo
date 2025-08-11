bbe2fd47ea15b7a39019582c8cd4ccf944453a33
Espen Gulbrandsen <espen.gulbrandsen@digdir.no>
2025-08-11 12:49:56 +0200

    Add required to property

--- ConsoleApp1/MessageDto.cs
+++ ConsoleApp1/MessageDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace ConsoleApp1
 {
     public record MessageDto
     {
-        public string Message { get; set; }
+        public required string Message { get; set; }
         public int DequeueCount { get; set; }
     }
 }
